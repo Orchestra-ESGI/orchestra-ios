c268db99aabdde7527696de664d5d74cd11e0479
Nassim Morouche <nassim.morouche1@gmail.com>
2021-07-23 16:52:32 +0200

    Open app from deeplink

--- Orchestra/Orchestra/Ressources/AppDelegate.swift
+++ Orchestra/Orchestra/Ressources/AppDelegate.swift
@@ -39,6 +39,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUser
         return true
     }
     
+    func application(_ application: UIApplication,
+                     continue userActivity: NSUserActivity,
+                     restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        
+        // Get URL components from the incoming user activity.
+        guard userActivity.activityType == NSUserActivityTypeBrowsingWeb,
+            let incomingURL = userActivity.webpageURL,
+            let components = NSURLComponents(url: incomingURL, resolvingAgainstBaseURL: true) else {
+            return false
+        }
+
+        // Check for specific URL components that you need.
+        guard let path = components.path,
+        let params = components.queryItems else {
+            return false
+        }
+        print("path = \(path)")
+
+        if let albumName = params.first(where: { $0.name == "albumname" } )?.value,
+            let photoIndex = params.first(where: { $0.name == "index" })?.value {
+
+            print("album = \(albumName)")
+            print("photoIndex = \(photoIndex)")
+            return true
+
+        } else {
+            print("Either album name or photo index missing")
+            return false
+        }
+    }
+    
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
         print("Firebase registration token: \(String(describing: fcmToken))")
         Messaging.messaging().token { token, error in
